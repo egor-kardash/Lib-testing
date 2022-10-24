a29380eb89b91b847376da5b6676ebc162b1c93a
Fritterus <mr.egorka.kardash.1999@gmail.com>
2022-10-24 13:37:59 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
+import { useState, ChangeEvent } from 'react';
 import { ToastProvider, toastService } from 'react-toast-k-lib';
-import { Button } from './styled';
+import { Button, Select } from './styled';
+
+type ToastType = 'Success' | 'Info' | 'Warning' | 'Error';
 
 const App = () => {
-  const handleClick = () => {
+  const [toastType, setToastType] = useState<ToastType>('Success');
+
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setToastType(e.target.value as ToastType);
+  }
+
+  const handleSuccessNotify = () => {
     toastService.addToast({
-      type: 'Info',
+      type: toastType,
       description: 'Toast Example'
     })
   }
 
   return (
     <div>
-      <Button onClick={handleClick}>Show toast</Button>
+      <Select value={toastType} onChange={handleChange}>
+        <option value="Success">Success</option>
+        <option value="Info">Info</option>
+        <option value="Warning">Warning</option>
+        <option value="Error">Error</option>
+      </Select>
+      <Button onClick={handleSuccessNotify}>Show toast</Button>
       <ToastProvider />
     </div>
   );
